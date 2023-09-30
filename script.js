fbef9ec850cb3a3326cfc4fd2613b39cf49f0903
Arun <arunpanigrahi18@gmail.com>
2023-09-30 18:06:59 +0530

    add more calculation options

--- script.js
+++ script.js
@@ -1,57 +1,84 @@
-const heading=document.getElementById('heading');
-heading.innerHTML="Enter two numbers";
-
 const num1input=document.getElementById('num1');
 const num2input=document.getElementById('num2');
-const calculateBtn=document.getElementById('calculateBtn');
-const answerArea=document.getElementById('answerArea');
+const result=document.getElementById('result');
 const calType=document.getElementById('calType');
-const addBtn=document.getElementById('addBtn');
-const subBtn=document.getElementById('subBtn');
-const mulBtn=document.getElementById('mulBtn');
-const divBtn=document.getElementById('divBtn');
 
-let calculateType="";
-calType.innerHTML=calculateType;
+const calculateBtn=document.getElementById('calculateBtn');
 
 const allBtns=document.querySelectorAll('.btn');
-
 allBtns.forEach((btn)=>{
     btn.addEventListener('click',()=>{
-        calculateType=btn.innerHTML;
-        calType.innerHTML=calculateType;
+        calType.value=btn.innerHTML;
     })
 })
+
+function add(num1,num2){
+    return num1+num2;
+}
+function sub(num1,num2){
+    return num1-num2;
+}
+function mul(num1,num2){
+    return num1*num2;
+}
+function div(num1,num2){
+    return (num1/num2).toFixed(2);
+}
+function pow(num1,num2){
+    return Math.pow(num1,num2);
+}
+function fact(num){
+    if(num===0) return 1;
+    let ans=1;
+    for(var i=1;i<=num;i++){
+        ans=ans*i;
+    }
+    return ans;
+}
+function permute(num1,num2){
+    if(num2>num1) return 0;
+    let numerator=fact(num1);
+    let denominator=fact(num1-num2);
+    return numerator/denominator;
+}
+function combine(num1,num2){
+    if(num2>num1) return 0;
+    let numerator=fact(num1);
+    let denominator=fact(num2)*fact(num1-num2);
+    return numerator/denominator;
+}
 
 function calculate(){
     let num1=Number(num1input.value);
     let num2=Number(num2input.value);
     let ans=0;
+    let calculateType=calType.value;
     switch(calculateType){
         case '+' :
-            ans=num1+num2;
+            ans=add(num1,num2);
             break;
         case '-' :
-            ans=num1-num2;
+            ans=sub(num1,num2);
             break;
         case 'x' :
-            ans=num1*num2;
+            ans=mul(num1,num2);
             break;
         case '/' :
-            ans=num1/num2;
+            ans=div(num1,num2);
+            break;
+        case '^' :
+            ans=pow(num1,num2);
+            break;
+        case 'P' :
+            ans=permute(num1,num2);
+            break;
+        case 'C' :
+            ans=combine(num1,num2);
             break;
     }
-    answerArea.innerHTML=`Result is ${ans}`;
+    result.value=ans;
 }
 
 calculateBtn.addEventListener('click',()=>{
     calculate();
 })
-
-// const getJoke=async()=>{
-
-// }
-
-// window.addEventListener('load',()=>{
-//     getJoke();
-// })
